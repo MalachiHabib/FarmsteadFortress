9e5a21ba4736935416986260d7a1b0e422140973
Malachi <malachi.habib@outlook.com>
2023-05-05 17:24:35 +0930

    init

---
+++ android/src/com/gamegroup/farmstedfortress/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.gamegroup.farmstedfortress;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.gamegroup.farmstedfortress.MyGdxGame;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new MyGdxGame(), config);
+	}
+}
