791eb15e560403d6ba0af0d84e490d814bc7ce72
Malachi <malachi.habib@outlook.com>
2023-05-07 20:31:49 +0930

    Refactored gameclass name

--- core/src/com/gamegroup/farmstedfortress/com/farmstedfortress/FarmstedFortress.java
+++ core/src/com/gamegroup/farmstedfortress/com/farmstedfortress/FarmstedFortress.java
@@ -1,15 +1,9 @@
 package com.farmstedfortress;
 
-import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Game;
-import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.OrthographicCamera;
-import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.math.Vector3;
-import com.badlogic.gdx.utils.ScreenUtils;
 
-public class MyGdxGame extends Game {
+public class FarmstedFortress extends Game {
     private SpriteBatch batch;
     private GameScreen screen;
 
