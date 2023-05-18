71f0fd5fb7afa377bf3e8dfb68d9d38b1a566e7b
Malachi Habib <Malachi.habib@outlook.com>
2023-05-18 22:21:30 +0930

    Changed enemy class to abstract, moved existing logic for the enemy into a temporary BasicEnemy class

---
+++ core/src/com/gamegroup/farmstedfortress/com/farmsteadfortress/entities/enemies/EnemyFactory.java
@@ -0,0 +1,56 @@
+package com.farmsteadfortress.entities.enemies;
+
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.Texture.TextureFilter;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+
+/**
+ * Factory class for creating Enemy instances.
+ */
+public class EnemyFactory {
+
+    private static final float DEFAULT_ANIMATION_SPEED = 1 / 10f;
+    private static final float DEFAULT_ENEMY_SPEED = 120f;
+    private static final String BASIC_ENEMY_TEXTURE_ATLAS_PATH = "entities/player/playerAtlas.atlas";
+
+    public enum EnemyType {
+        BASIC_ENEMY,
+    }
+
+    /**
+     * Creates a new Enemy instance with default settings based on the type.
+     *
+     * @param type The type of enemy to create.
+     * @return The created Enemy instance.
+     */
+    public static Enemy createEnemy(EnemyType type) {
+        switch (type) {
+            case BASIC_ENEMY:
+                return createBasicEnemy();
+            default:
+                return null;
+        }
+    }
+
+    /**
+     * Creates a BasicEnemy instance.
+     *
+     * @return The created BasicEnemy instance.
+     */
+    private static BasicEnemy createBasicEnemy() {
+        TextureAtlas atlas = new TextureAtlas(BASIC_ENEMY_TEXTURE_ATLAS_PATH);
+        setTextureFilters(atlas);
+        return new BasicEnemy(atlas, DEFAULT_ANIMATION_SPEED, DEFAULT_ENEMY_SPEED, 20);
+    }
+
+    /**
+     * Set texture filters for all textures in the atlas.
+     *
+     * @param atlas The texture atlas.
+     */
+    private static void setTextureFilters(TextureAtlas atlas) {
+        for (Texture texture : atlas.getTextures()) {
+            texture.setFilter(TextureFilter.Linear, TextureFilter.Linear);
+        }
+    }
+}
